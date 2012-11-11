13592a7ca0ad7b5b05272269182801d47384c3cc
Graham Jones <grahamjones139@gmail.com>
2012-11-11 19:26:19 +0000

    Added description to top of main source file for solThMon.

--- solThMon/solThMon.ino
+++ solThMon/solThMon.ino
@@ -1,3 +1,31 @@
+/**
+ * solThMon - Arduino based solar thermal power monitor.
+ * The power being extracted from a solar collector is determined
+ * by measuring the temperature difference in the primary circuit
+ * water across the solar collector, and estimating the water
+ * flow rate.
+ * 
+ * The collector water inlet and outlet temperature is measured
+ * using Dallas 18B20 one wire digital temperature sensors.
+ * 
+ * The water flow rate is set by a separate solar controller.
+ * The pump speed is set by modulating the current to the pump
+ * motor.   The load factor (=proportion of full power) of
+ * the pump is measured by sampling the motor supply voltage
+ * many times (~10000) and checking the proportion of the samples
+ * at approximately zero volts.   The actual flow rate is taken to
+ * be the load factor multiplied by a calibration factor that
+ * has been determined separately by measuring the flow rate
+ * using a separate meter.
+ *
+ * The instantaneous power is then simply P = mCp(T2-T1), where
+ * Cp is the specific heat capacity of water (~4.2 kJ/kg/K), m
+ * is the mass flow rate (kg/s) and T1 and T2 are the collector
+ * inlet and outlet temperatures respectively (K or degC).
+ *
+ * Copyright Graham Jones, 2012
+ *
+ */
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <LiquidCrystal.h>
@@ -6,7 +34,7 @@
 #define ONE_WIRE_BUS 2
 #define TEMPERATURE_PRECISION 9
 #define NSAMPLES 10000   // Number of analog samples to collect for
-                      // load factor calculation.
+// load factor calculation.
 int acVoltsPin = A0;  // Pin connected to AC voltage signal.
 
 LiquidCrystal lcd(12,11,10,6,5,4,3);
@@ -32,13 +60,13 @@ void setup(void)
 
   // Start up the library
   sensors.begin();
-  
+
   // Grab a count of devices on the wire
   numberOfDevices = sensors.getDeviceCount();
-  
+
   // locate devices on the bus
   Serial.print("Locating devices...");
-  
+
   Serial.print("Found ");
   Serial.print(numberOfDevices, DEC);
   Serial.println(" devices.");
@@ -47,33 +75,34 @@ void setup(void)
   Serial.print("Parasite power is: "); 
   if (sensors.isParasitePowerMode()) Serial.println("ON");
   else Serial.println("OFF");
-  
+
   // Loop through each device, print out address
   for(int i=0;i<numberOfDevices; i++)
   {
     // Search the wire for address
     if(sensors.getAddress(tempDeviceAddress, i))
-	{
-		Serial.print("Found device ");
-		Serial.print(i, DEC);
-		Serial.print(" with address: ");
-		printAddress(tempDeviceAddress);
-		Serial.println();
-		
-		Serial.print("Setting resolution to ");
-		Serial.println(TEMPERATURE_PRECISION, DEC);
-		
-		// set the resolution to TEMPERATURE_PRECISION bit (Each Dallas/Maxim device is capable of several different resolutions)
-		sensors.setResolution(tempDeviceAddress, TEMPERATURE_PRECISION);
-		
-		 Serial.print("Resolution actually set to: ");
-		Serial.print(sensors.getResolution(tempDeviceAddress), DEC); 
-		Serial.println();
-	}else{
-		Serial.print("Found ghost device at ");
-		Serial.print(i, DEC);
-		Serial.print(" but could not detect address. Check power and cabling");
-	}
+    {
+      Serial.print("Found device ");
+      Serial.print(i, DEC);
+      Serial.print(" with address: ");
+      printAddress(tempDeviceAddress);
+      Serial.println();
+
+      Serial.print("Setting resolution to ");
+      Serial.println(TEMPERATURE_PRECISION, DEC);
+
+      // set the resolution to TEMPERATURE_PRECISION bit (Each Dallas/Maxim device is capable of several different resolutions)
+      sensors.setResolution(tempDeviceAddress, TEMPERATURE_PRECISION);
+
+      Serial.print("Resolution actually set to: ");
+      Serial.print(sensors.getResolution(tempDeviceAddress), DEC); 
+      Serial.println();
+    }
+    else{
+      Serial.print("Found ghost device at ");
+      Serial.print(i, DEC);
+      Serial.print(" but could not detect address. Check power and cabling");
+    }
   }
 
 }
@@ -127,30 +156,30 @@ void loop(void)
   float loadFactor;
   // call sensors.requestTemperatures() to issue a global temperature 
   // request to all devices on the bus
-  
+
   lcd.setCursor(0,1);
   lcd.print(millis()/1000);
   Serial.print("Requesting temperatures...");
   sensors.requestTemperatures(); // Send the command to get temperatures
   Serial.println("DONE");
-  
-  
+
+
   // Loop through each device, print out temperature data
   for(int i=0;i<numberOfDevices; i++)
   {
     // Search the wire for address
     if(sensors.getAddress(tempDeviceAddress, i))
-	{
-		// Output the device ID
-		Serial.print("Temperature for device: ");
-		Serial.print(i,DEC);
-                Serial.print(" - ");
-		
-		// It responds almost immediately. Let's print out the data
-		printTemperature(tempDeviceAddress); // Use a simple function to print out the data
-	} 
-	//else ghost device! Check your power requirements and cabling
-	
+    {
+      // Output the device ID
+      Serial.print("Temperature for device: ");
+      Serial.print(i,DEC);
+      Serial.print(" - ");
+
+      // It responds almost immediately. Let's print out the data
+      printTemperature(tempDeviceAddress); // Use a simple function to print out the data
+    } 
+    //else ghost device! Check your power requirements and cabling
+
   }
   loadFactor = getLoadFactor();
   Serial.print("Load Factor = ");
@@ -167,3 +196,4 @@ void printAddress(DeviceAddress deviceAddress)
     Serial.print(deviceAddress[i], HEX);
   }
 }
+
