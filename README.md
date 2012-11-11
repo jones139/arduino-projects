fc3e6544bc47aa1d38f1ac00dbcfc5006c6c7208
Graham Jones <grahamjones139@gmail.com>
2012-11-11 15:22:58 +0000

    First commit of outline Solar Thermal Monitor project.

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ arduino-projects
 ================
 
 A collection of projects for arduino, with associated libraries collected together into one place.
+
+The projects included here are:
+
+solThMon - Solar Thermal Monitor - displays instantaneous power extracted from a solar thermal collector - measures the collector inlet temperature, outlet temperature and water flow rate to deduce the power and display it on an LCD display.
+
+
+The libraries included from other projects are:
+
+OneWire by Paul Stoffregen - http://www.arduino.cc/playground/Learning/OneWire
+
+DallasTemperature by Miles Burton - http://milesburton.com/Dallas_Temperature_Control_Library
---
+++ solThMon/solThMon.ino
@@ -0,0 +1,169 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <LiquidCrystal.h>
+
+// Data wire is plugged into port 2 on the Arduino
+#define ONE_WIRE_BUS 2
+#define TEMPERATURE_PRECISION 9
+#define NSAMPLES 10000   // Number of analog samples to collect for
+                      // load factor calculation.
+int acVoltsPin = A0;  // Pin connected to AC voltage signal.
+
+LiquidCrystal lcd(12,11,10,6,5,4,3);
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+
+int numberOfDevices; // Number of temperature devices found
+
+DeviceAddress tempDeviceAddress; // We'll use this variable to store a found device address
+
+void setup(void)
+{
+  // start serial port
+  Serial.begin(9600);
+  Serial.println("Dallas Temperature IC Control Library Demo");
+
+  lcd.begin(16,2);
+  lcd.print("hello World!!");
+
+  // Start up the library
+  sensors.begin();
+  
+  // Grab a count of devices on the wire
+  numberOfDevices = sensors.getDeviceCount();
+  
+  // locate devices on the bus
+  Serial.print("Locating devices...");
+  
+  Serial.print("Found ");
+  Serial.print(numberOfDevices, DEC);
+  Serial.println(" devices.");
+
+  // report parasite power requirements
+  Serial.print("Parasite power is: "); 
+  if (sensors.isParasitePowerMode()) Serial.println("ON");
+  else Serial.println("OFF");
+  
+  // Loop through each device, print out address
+  for(int i=0;i<numberOfDevices; i++)
+  {
+    // Search the wire for address
+    if(sensors.getAddress(tempDeviceAddress, i))
+	{
+		Serial.print("Found device ");
+		Serial.print(i, DEC);
+		Serial.print(" with address: ");
+		printAddress(tempDeviceAddress);
+		Serial.println();
+		
+		Serial.print("Setting resolution to ");
+		Serial.println(TEMPERATURE_PRECISION, DEC);
+		
+		// set the resolution to TEMPERATURE_PRECISION bit (Each Dallas/Maxim device is capable of several different resolutions)
+		sensors.setResolution(tempDeviceAddress, TEMPERATURE_PRECISION);
+		
+		 Serial.print("Resolution actually set to: ");
+		Serial.print(sensors.getResolution(tempDeviceAddress), DEC); 
+		Serial.println();
+	}else{
+		Serial.print("Found ghost device at ");
+		Serial.print(i, DEC);
+		Serial.print(" but could not detect address. Check power and cabling");
+	}
+  }
+
+}
+
+// function to print the temperature for a device
+void printTemperature(DeviceAddress deviceAddress)
+{
+  // method 1 - slower
+  //Serial.print("Temp C: ");
+  //Serial.print(sensors.getTempC(deviceAddress));
+  //Serial.print(" Temp F: ");
+  //Serial.print(sensors.getTempF(deviceAddress)); // Makes a second call to getTempC and then converts to Fahrenheit
+
+  // method 2 - faster
+  float tempC = sensors.getTempC(deviceAddress);
+  Serial.print("Temp C: ");
+  Serial.println(tempC);
+}
+
+
+float getLoadFactor(void) {
+  int n;
+  int val;
+  int nLowSamples = 0;
+  float total = 0.0;
+  float mean;
+  float loadFactor;
+  for (n=0;n<NSAMPLES;n++) {
+    val  = analogRead(acVoltsPin);
+    total +=val;
+    if (val<5) nLowSamples++;
+  }
+  mean = total/NSAMPLES;
+  loadFactor = 1.0* (NSAMPLES-nLowSamples) / NSAMPLES;
+  Serial.println("getLoadFactor");
+  Serial.print("mean=");
+  Serial.print(mean);
+  Serial.println(".");
+  Serial.print("nLowSamples=");
+  Serial.print(nLowSamples);
+  Serial.println(".");
+  return(loadFactor);
+}
+
+
+
+
+
+void loop(void)
+{ 
+  float loadFactor;
+  // call sensors.requestTemperatures() to issue a global temperature 
+  // request to all devices on the bus
+  
+  lcd.setCursor(0,1);
+  lcd.print(millis()/1000);
+  Serial.print("Requesting temperatures...");
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  Serial.println("DONE");
+  
+  
+  // Loop through each device, print out temperature data
+  for(int i=0;i<numberOfDevices; i++)
+  {
+    // Search the wire for address
+    if(sensors.getAddress(tempDeviceAddress, i))
+	{
+		// Output the device ID
+		Serial.print("Temperature for device: ");
+		Serial.print(i,DEC);
+                Serial.print(" - ");
+		
+		// It responds almost immediately. Let's print out the data
+		printTemperature(tempDeviceAddress); // Use a simple function to print out the data
+	} 
+	//else ghost device! Check your power requirements and cabling
+	
+  }
+  loadFactor = getLoadFactor();
+  Serial.print("Load Factor = ");
+  Serial.println(loadFactor);
+  delay(1000);
+}
+
+// function to print a device address
+void printAddress(DeviceAddress deviceAddress)
+{
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    if (deviceAddress[i] < 16) Serial.print("0");
+    Serial.print(deviceAddress[i], HEX);
+  }
+}
