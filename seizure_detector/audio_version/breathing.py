5b5efcf4619feaf5ca5c7056149996ee3e9e991f
Graham Jones <grahamjones139@gmail.com>
2013-02-23 21:34:12 +0000

    Modified breathing detector to reduce amount of data.

--- seizure_detector/audio_version/breathing.py
+++ seizure_detector/audio_version/breathing.py
@@ -13,11 +13,15 @@ import time
 import numpy, scipy, scipy.fftpack
 import pylab
 
+sampleFreq = 100   # Hz
+samplePeriod = 10   # sec
+nSamples = sampleFreq * samplePeriod
+
 inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE,alsaaudio.PCM_NONBLOCK)
 
 # Set attributes: Mono, 8000 Hz, 16 bit little endian samples
 inp.setchannels(1)
-inp.setrate(8000)
+inp.setrate(sampleFreq)
 inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)
 
 # The period size controls the internal number of frames per period.
@@ -29,23 +33,27 @@ inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)
 # mode.
 inp.setperiodsize(160)
 
-analysis_period = 10    # seconds
+analysis_period = samplePeriod    # seconds
 t_start = time.time()
 samples = [];
 counter = 0
 
 while True:
 	# Collect data for analysis_period seconds, then analyse it.
-	if ((time.time()-t_start) > analysis_period):
+	#if ((time.time()-t_start) > analysis_period):
+	if (len(samples)>=nSamples):
 		print "analysis time!"
 		sample_array = numpy.array(samples)
 		sample_fft = scipy.fft(samples)
 		offt = open('outfile_fft.dat','w')
 		freqs = []
+		times = []
 		sample_no = []
 		sn = 0
+		freqBinWidth = 1.0*sampleFreq/len(samples)
 		for x in range(len(sample_fft)):
-			freq = 1.0*x/analysis_period
+			times.append(1.0*x/sampleFreq)
+			freq = 1.0*x*freqBinWidth
 			freqs.append(freq)
 			sample_no.append(sn)
 			sn += 1
@@ -53,16 +61,24 @@ while True:
 				   (freq,
 				    abs(sample_fft[x].real)))
 
-		print len(freqs),len(sample_fft)
+		#print len(freqs),len(sample_fft)
+		print "Sample Frequency = %3.2f Hz " % (sampleFreq)
+		print "Number of Samples = %d" % (len(samples))
+		print "Frequency Resolution = %3.2f Hz" % (freqBinWidth)
 
 		#freqs = scipy.fftpack.fftfreq(signal.size, t[1]-t[0])
 
 		pylab.subplot(211)
-		pylab.plot(sample_no, samples)
+		pylab.plot(times, samples)
+		pylab.xlabel("time (s)")
+		pylab.ylabel("value");
 		pylab.subplot(212)
-		pylab.plot(freqs,20*scipy.log10(sample_fft))
-		#pylab.xlim(0,freqs[len(sample_fft)/2])
-		pylab.xlim(0,100)
+		pylab.plot(freqs,sample_fft)
+		pylab.xlabel("freq (Hz)")
+		pylab.ylabel("amplitude")
+		pylab.xlim(0,freqs[len(sample_fft)/2])
+		pylab.ylim(0)
+		#pylab.xlim(0,100)
 		pylab.show()
 		t_start = time.time()
 	else:
@@ -70,15 +86,15 @@ while True:
 		l,data = inp.read()
 		counter = 0
 		if l:
-			print "processing data"
-			print data
+			#print "processing data"
+			#print data
 			while 1:
 				try:
 					samples.append(audioop.getsample(data,2,counter))
 					counter += 1
 				except:
-					print "counter = %d" % (counter)
-					print "end of data"
+					#print "counter = %d" % (counter)
+					#print "end of data"
 					break
 			# Return the maximum of the absolute value of all samples in a fragment.
 			#print samples
