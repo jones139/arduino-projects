94e6bb31e74d477726e776f4ce5f310489d1aadb
Graham Jones <grahamjones139@gmail.com>
2013-11-17 20:44:40 +0000

    added interrupt method for bubble counter.

--- biogas/gpioTest.py
+++ biogas/gpioTest.py
@@ -4,12 +4,23 @@ from time import sleep
 import os
 import RPi.GPIO as GPIO
 
+bubblePin = 17
+bubbleCount = 0
+
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.IN)
+GPIO.setup(bubblePin, GPIO.IN)
+
+def bubbleDetected(channel):
+    print("bubble detected")
+    bubbleCount += 1
+
+GPIO.add_event_detect(bubblePin,GPIO.RISING, callback=bubbleDetected)
+
 
 while True:
-    if (GPIO.input(17) == False):
-        print("False")
-    else:
-        print("True")
-
+#    if (GPIO.input(bubblePin) == False):
+#        print("False")
+#    else:
+#        print("True")
+    print("Bubble Count = %d\n" % bubbleCount)
+    sleep(1000)
