4be1ac68fca1a47d3917a54f29f0ac20a28d7cb0
Graham Jones <grahamjones139@gmail.com>
2016-05-21 21:28:10 +0100

    Starting point for turntable controller - just sweeps in one direction then the other.

---
+++ Alans_Turntable/Alans_Turntable.ino
@@ -0,0 +1,20 @@
+void setup()
+{
+pinMode(2,OUTPUT);
+pinMode(3,OUTPUT);
+}
+
+void loop()
+{
+  // reverse direction of rotation.
+  digitalWrite(3,!(digitalRead(3)));
+
+  // The number in the for loop sets how far it rotates before changing direction.
+  for (int I=0;I<10000;I=I+1) {
+     digitalWrite(2,HIGH);
+     digitalWrite(2,LOW) ;
+     // Delay sets the speed - if it is less than 300 the motor stutters and does not move.
+     // larger number = slower rotation.
+     delayMicroseconds(350);
+  }
+}
